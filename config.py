8f67ab55b1beff4970465fb916d5da10c905394d
maryemackin <140846872+maryemackin@users.noreply.github.com>
2024-04-10 22:05:06 -0700

    updated files

---
+++ config.py
@@ -0,0 +1,13 @@
+weather_api_key = "6e4fff24d029c4f2cce6a0af4265f675"
+
+omdb_api_key = "b8debd85"
+
+nyt_api_key="fpgNJQMMAf0Lpw0QKSb0hMdtcxUnMHim"
+
+geoapify_key = "fe54b47f7c6749be89776313f2bab541"
+
+#data.gov api key
+api_key = "KxyInVa4afnkcvCGVsS3Atb4PFZPUciVlnJWlhO0"
+
+crimometer_api_key = "k3RAzKN1Ag14xTPlculT39RZb38LGgsG8n27ZycG"
+
